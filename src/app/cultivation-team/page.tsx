19376d0deeea9a0bccdb7ad92d6ce997a2588a77
Viktar Rudzenia <viktarrudzenia@gmail.com>
2024-09-08 13:00:59 +0200

    feat: added placeholder table to the Cultivation team

--- src/app/cultivation-team/page.tsx
+++ src/app/cultivation-team/page.tsx
@@ -1,5 +1,11 @@
+import CultivationTeamTable from '@/components/cultivation-team/CultivationTeamTable';
+
 import styles from './page.module.scss';
 
 export default function CultivationTeam() {
-  return <div className={styles.wrapper}>Cultivation team page</div>;
+  return (
+    <div className={styles.wrapper}>
+      <CultivationTeamTable />
+    </div>
+  );
 }
