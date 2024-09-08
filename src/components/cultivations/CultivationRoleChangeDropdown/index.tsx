d0516aaf67e59613dbe0994ca61269986a5a240c
Viktar Rudzenia <viktarrudzenia@gmail.com>
2024-09-08 16:24:57 +0200

    feat: added modal before changing role

--- src/components/cultivations/CultivationRoleChangeDropdown/index.tsx
+++ src/components/cultivations/CultivationRoleChangeDropdown/index.tsx
@@ -1,5 +1,6 @@
+import { MouseEvent, useState } from 'react';
 import { DownOutlined } from '@ant-design/icons';
-import { Button, Dropdown } from 'antd';
+import { Button, Dropdown, Modal } from 'antd';
 
 import { CultivationRolesInterface } from '@/utils/interfaces';
 
@@ -12,25 +13,55 @@ export default function CultivationRoleChangeDropdown({
   cultivationRolesObj: { [key: string]: CultivationRolesInterface };
   roleId: number;
 }) {
-  const roleOptions = [
-    {
-      label: '1st menu item',
-      key: '0',
-    },
-    {
-      label: '2nd menu item',
-      key: '1',
-    },
-    {
-      label: '3rd menu item',
-      key: '3',
-    },
-  ];
+  const [cultivationRoleToChange, setCultivationRoleToChange] =
+    useState<CultivationRolesInterface | null>(null);
+
+  const handleDropdownChange = ({
+    event,
+    cultivationRole,
+  }: {
+    event: MouseEvent;
+    cultivationRole: CultivationRolesInterface;
+  }) => {
+    event.preventDefault();
+    if (roleId !== cultivationRole.id) {
+      setCultivationRoleToChange(cultivationRole);
+    }
+  };
+
+  const roleOptions = Object.values(cultivationRolesObj).map((cultivationRole) => ({
+    label: (
+      <a onClick={(e) => handleDropdownChange({ event: e, cultivationRole: cultivationRole })}>
+        {cultivationRole.name}
+      </a>
+    ),
+    key: cultivationRole.id,
+  }));
+
+  const handleChangeUserRole = () => {
+    setCultivationRoleToChange(null);
+  };
+
   return (
-    <Dropdown className={styles.dropdown} menu={{ items: roleOptions }} trigger={['click']}>
-      <Button>
-        {cultivationRolesObj[roleId].name} <DownOutlined />
-      </Button>
-    </Dropdown>
+    <>
+      <Dropdown className={styles.dropdown} menu={{ items: roleOptions }} trigger={['click']}>
+        <Button>
+          {cultivationRolesObj[roleId].name} <DownOutlined />
+        </Button>
+      </Dropdown>
+      <Modal
+        title="Change Role"
+        open={!!cultivationRoleToChange}
+        onOk={handleChangeUserRole}
+        onCancel={() => setCultivationRoleToChange(null)}
+        centered
+        okText="Change"
+      >
+        <div>
+          Do you really want change role from {cultivationRolesObj[roleId].name} to{' '}
+          {cultivationRoleToChange && cultivationRolesObj[cultivationRoleToChange.id].name}
+        </div>
+      </Modal>
+    </>
   );
 }
