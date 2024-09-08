9e7a10ec367a51fa05db5e4b8af909a3e9996105
Viktar Rudzenia <viktarrudzenia@gmail.com>
2024-09-08 20:00:31 +0200

    feat: implemented add users to the Cultivation team functionality, including multiple users adding

--- src/components/cultivations/AddUsersToCultivation/index.tsx
+++ src/components/cultivations/AddUsersToCultivation/index.tsx
@@ -1,8 +1,20 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { Button, Input, Result, Spin, Table, TableColumnsType, TableProps } from 'antd';
-import useSWR from 'swr';
+import {
+  Button,
+  Input,
+  Modal,
+  Result,
+  Spin,
+  Table,
+  TableColumnsType,
+  TableProps,
+  notification,
+} from 'antd';
+import useSWR, { mutate } from 'swr';
+import { useParams } from 'next/navigation';
+import axios from 'axios';
 
 import { fetcher } from '@/utils/fetcher';
 import { ApiRoutes } from '@/utils/constants';
@@ -11,15 +23,26 @@ import { UserInterface } from '@/utils/interfaces';
 import styles from './index.module.scss';
 
 type TableRowSelection<T extends object = object> = TableProps<T>['rowSelection'];
+enum AddUsersRequestType {
+  SINGLE = 'single',
+  MULTIPLE = 'multiple',
+}
 
 export default function AddUsersToCultivation({
   setIsAddUserModalOpen,
 }: {
   setIsAddUserModalOpen: (value: boolean) => void;
 }) {
+  const { cultivationId }: { cultivationId: string } = useParams();
+
+  const [notificationApi, notificationApiContextHolder] = notification.useNotification();
   const [searchUser, setSearchUser] = useState<string>('');
   const [usersDataWithAppliedSearch, setUsersDataWithAppliedSearch] = useState<UserInterface[]>([]);
   const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const [isAddOneUserModalOpen, setIsAddOneUserModalOpen] = useState<boolean>(false);
+  const [isMultipleUsersModalOpen, setIsMultipleUsersModalOpen] = useState<boolean>(false);
+  const [isAddUsersToCultivationLoading, setIsAddUsersToCultivationLoading] =
+    useState<boolean>(false);
 
   const {
     data: usersData,
@@ -44,24 +67,54 @@ export default function AddUsersToCultivation({
     },
   ];
 
-  const handleSearchUser = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchUser(e.target.value);
-  };
-
-  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
-    console.log('selectedRowKeys changed: ', newSelectedRowKeys);
-    setSelectedRowKeys(newSelectedRowKeys);
-  };
-
   const rowSelection: TableRowSelection<UserInterface> = {
     selectedRowKeys,
-    onChange: onSelectChange,
+    onChange: (newSelectedRowKeys: React.Key[]) => setSelectedRowKeys(newSelectedRowKeys),
   };
 
-  const hasSelected = selectedRowKeys.length > 0;
+  const hasUsersSelected = selectedRowKeys.length > 0;
+
+  const handleAddUsersToCultivationTeam = async ({ type }: { type: AddUsersRequestType }) => {
+    setIsAddUsersToCultivationLoading(true);
+    try {
+      await axios.post(
+        ApiRoutes.getUsersInCultivationApi(cultivationId),
+        type === AddUsersRequestType.SINGLE
+          ? {
+              role: { id: 3 },
+              user: { id: selectedRowKeys[0] },
+            }
+          : [selectedRowKeys.map((userId) => ({ role: { id: 3 }, user: { id: userId } }))]
+      );
+      notificationApi.success({
+        message: type === AddUsersRequestType.SINGLE ? 'User added' : 'Users Added',
+        description:
+          type === AddUsersRequestType.SINGLE
+            ? 'User has been successfully added to Cultivation Team'
+            : `${selectedRowKeys.length} Users has been successfully added to Cultivation Team`,
+      });
+
+      mutate(ApiRoutes.getUsersInCultivationApi(cultivationId));
+      setIsAddUserModalOpen(false);
+      setSelectedRowKeys([]);
+    } catch (err) {
+      notificationApi.error({
+        message: 'Error',
+        description:
+          type === AddUsersRequestType.SINGLE
+            ? 'An error occurred while adding the user to the Cultivation team'
+            : `Error occurred during adding ${selectedRowKeys.length} Users to Cultivation Team`,
+      });
+    }
+    setIsAddUsersToCultivationLoading(false);
+    type === AddUsersRequestType.SINGLE
+      ? setIsAddOneUserModalOpen(false)
+      : setIsMultipleUsersModalOpen(false);
+  };
 
   return (
     <div className={styles.wrapper}>
+      {notificationApiContextHolder}
       {isUsersDataLoading && <Spin size="large" />}
 
       {!isUsersDataLoading && userDataError && (
@@ -86,7 +139,7 @@ export default function AddUsersToCultivation({
             enterButton
             value={searchUser}
             placeholder="Search teammember"
-            onChange={handleSearchUser}
+            onChange={(e) => setSearchUser(e.target.value)}
           />
           <Table
             rowKey="id"
@@ -101,15 +154,23 @@ export default function AddUsersToCultivation({
             rowSelection={rowSelection}
           />
           <div className={styles.actionBtns}>
-            <Button disabled={!hasSelected} onClick={() => setSelectedRowKeys([])}>
+            <Button disabled={!hasUsersSelected} onClick={() => setSelectedRowKeys([])}>
               Reset Selection
             </Button>
             <Button
-              disabled={!hasSelected}
+              disabled={!hasUsersSelected}
               type="primary"
-              onClick={() => setIsAddUserModalOpen(false)}
+              onClick={() => setIsMultipleUsersModalOpen(true)}
             >
-              Add to cultivation team {hasSelected && `${selectedRowKeys.length} users`}
+              (API NEED TO BE UPDATED) Add multiple users:{' '}
+              {hasUsersSelected && `${selectedRowKeys.length} users`}
+            </Button>
+            <Button
+              disabled={!hasUsersSelected}
+              type="primary"
+              onClick={() => setIsAddOneUserModalOpen(true)}
+            >
+              Add 1 user to cultivation team, first selected
             </Button>
           </div>
         </>
@@ -118,6 +179,30 @@ export default function AddUsersToCultivation({
       {!isUsersDataLoading && !userDataError && usersData?.length === 0 && (
         <div className={styles.emptyData}>Unfortunately, no Users were found.</div>
       )}
+
+      <Modal
+        title="Add Users to Cultivation Team"
+        open={!!isMultipleUsersModalOpen}
+        onOk={() => handleAddUsersToCultivationTeam({ type: AddUsersRequestType.MULTIPLE })}
+        onCancel={() => setIsMultipleUsersModalOpen(false)}
+        okText="Add users"
+        centered
+        okButtonProps={{ loading: isAddUsersToCultivationLoading }}
+      >
+        <div>Do you really want to add {selectedRowKeys.length} Users to Cultivation Team?</div>
+      </Modal>
+
+      <Modal
+        title="Add User to Cultivation Team"
+        open={!!isAddOneUserModalOpen}
+        onOk={() => handleAddUsersToCultivationTeam({ type: AddUsersRequestType.SINGLE })}
+        onCancel={() => setIsAddOneUserModalOpen(false)}
+        okText="Add 1 user"
+        centered
+        okButtonProps={{ loading: isAddUsersToCultivationLoading }}
+      >
+        <div>Do you really want to add 1 User to Cultivation Team?</div>
+      </Modal>
     </div>
   );
 }
