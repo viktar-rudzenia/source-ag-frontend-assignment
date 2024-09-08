dc35d593434310c1ca199c33ffadca46509b6bb2
Viktar Rudzenia <viktarrudzenia@gmail.com>
2024-09-08 12:44:30 +0200

    feat: created ApiRoutes constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,27 @@ export enum AllRoutesEnum {
   HOME = '/',
   CULTIVATION_TEAM = '/cultivation-team',
 }
+
+const API_URL = 'https://14dtv3lu9k.execute-api.eu-central-1.amazonaws.com';
+const CULTIVATION_ID_TEXT = '<cultivation_id>';
+const USER_ID_TEXT = '<user_id>';
+
+export const ApiRoutes = {
+  cultivations: `${API_URL}/cultivations`,
+  users: `${API_URL}/users`,
+  cultivationRoles: `${API_URL}/cultivation-roles`,
+  usersInCultivationPattern: `${API_URL}/${CULTIVATION_ID_TEXT}/users`,
+  changeUserInCultivationPattern: `${API_URL}/${CULTIVATION_ID_TEXT}/users/${USER_ID_TEXT}`,
+  getUsersInCultivationApi: (cultivationId: string) =>
+    ApiRoutes.usersInCultivationPattern.replace(CULTIVATION_ID_TEXT, cultivationId),
+  getChangeUserInCultivationApi: ({
+    cultivationId,
+    userId,
+  }: {
+    cultivationId: string;
+    userId: string;
+  }) =>
+    ApiRoutes.changeUserInCultivationPattern
+      .replace(CULTIVATION_ID_TEXT, cultivationId)
+      .replace(USER_ID_TEXT, userId),
+};
